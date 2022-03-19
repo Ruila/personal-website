3fbd1801d5a0f5c5ceb6b77ab7d976570e6fd6de
Ruila <4momolmr@@gmail.com>
2022-03-19 22:33:23 +0800

    add cloud

---
+++ src/components/mario/Brick.tsx
@@ -0,0 +1,21 @@
+import React from "react"
+
+type GroundType = {
+}
+
+export const Brick: React.FunctionComponent<GroundType> = () => {
+    const audio = new Audio("https://s3-us-west-2.amazonaws.com/s.cdpn.io/94617/coin.mp3")
+
+    const playCoinSound = () => {
+        audio.play();
+    }
+
+    return <div className="absolute bottom-[200px] left-[30%] w-[40px] h-[40px]" onClick={playCoinSound}>
+        <div className="w-full absolute z-[1]">
+            <img className="w-full" src={'images/mario_brick.png'}/>
+        </div>
+        <div className="absolute top-0 flex w-full justify-center coin">
+            <img className="w-[25px]" src="https://i.gifer.com/origin/50/509175766f72a32aeb3cde0de5114122_w200.gif" alt="" id="coin"/>
+        </div>
+    </div>
+}
--- src/components/mario/MarioView.tsx
+++ src/components/mario/MarioView.tsx
@@ -1,10 +1,25 @@
 import React from "react"
-import { Ground } from "./Ground";
+import { Ground } from "./Ground"
+import { Brick } from "./Brick"
 
 export const MarioView: React.FunctionComponent = () => {
-    return (
-        <div className="bg-[#5e94fb] h-available">
-            <Ground/>
-        </div>
-    )
+  return (
+    <div className="bg-[#5e94fb] h-available relative overflow-hidden">
+      <img
+        id="cloud1"
+        height="91"
+        width="220"
+        src="https://i.pinimg.com/originals/69/3c/73/693c7325f91ceebf4cd65d7c6eccf606.png"
+        alt="Large Mario Cloud"
+      />
+      <img
+        id="cloud2"
+        height="146"
+        width="228"
+        src="https://i.pinimg.com/originals/3a/02/91/3a02910047fbe517dc20dfee72a62a6b.png"
+      />
+      <Brick />
+      <Ground />
+    </div>
+  )
 }
