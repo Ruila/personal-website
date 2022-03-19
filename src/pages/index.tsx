29a468e939969afa81e87895b3d93ab2eaee3d29
William Lin <4momolmr@gmail.com>
2022-03-19 15:23:56 +0800

    chore: initialize projects (#1)
    * initialize
    
    * add eslint typescript prettier
    
    Co-authored-by: Ruila <4momolmr@@gmail.com>

---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const IndexPage = (): JSX.Element => {
+  return <p />
+}
+
+export default IndexPage
---
+++ src/pages/mario.tsx
@@ -0,0 +1,6 @@
+import React from "react"
+import {Ground} from "../components/mario/Ground";
+
+export const MarioPage: React.FunctionComponent = () => {
+    return <Ground name="aa"/>
+}
