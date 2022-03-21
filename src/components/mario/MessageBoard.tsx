acd3e1b7d9d6693e32e74093c998dc0e09d5819d
Ruila <4momolmr@gmail.com>
2022-03-21 22:57:51 +0800

    add font family

--- src/components/mario/MessageBoard.tsx
+++ src/components/mario/MessageBoard.tsx
@@ -9,11 +9,14 @@ export const MessageBoard: React.FunctionComponent<MessageBoardType> = ({
 }) => {
   return (
     <div
-      className={`absolute bg-[#de5a18] message_board ${
+      className={`absolute bg-[#de5a18] message_board font-press-star text-white p-4 ${
         show ? "message_board_animate" : ""
       }`}
     >
-      ss
+      <div>Profile</div>
+      <div className="my-4">LINKIN</div>
+      <div className="my-4">GITHUB</div>
+      <div className="my-4">CakeResume</div>
     </div>
   )
 }
